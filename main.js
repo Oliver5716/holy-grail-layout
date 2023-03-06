41e8e0b40d4ed52863379b17e03849acf56ad7a5
Oliver <125431055+Oliver5716@users.noreply.github.com>
2023-03-06 11:19:39 +0800

    commit files

---
+++ main.js
@@ -0,0 +1,14 @@
+function toggleNav() {
+    const outerGrid = document.querySelector(".outer-grid");
+    outerGrid.classList.toggle("outer-grid-expanded");
+
+    const nav = document.querySelector("nav");
+    nav.classList.toggle("hide-nav");
+}
+
+const displayCurrentYear = () => {
+    const year = new Date().getFullYear();
+    document.querySelector("#year").textContent = year;
+};
+
+displayCurrentYear();
